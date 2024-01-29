2dd97044e22730a41af38b148dc06a1020e3b462
Aditya Charan <77635268+PNAC03@users.noreply.github.com>
2024-01-29 20:43:10 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-x = `2 + 2`
-if (x = 4)
-echo "This is my first command"
-else
-echo "This is second"
+$x = `2 + 2`
+if ($x = 4)
+then echo "This is my first command"
+else echo "This is second"
 fi
